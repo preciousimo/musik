b68412165b81efd83f4c4f293f1de2d10be1e606
preciousimo <preciousimoniakemu@gmail.com>
2022-11-16 21:32:51 +0100

    Create error component

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-
 const Error = () => (
-  <div>Error</div>
+  <div className="w-full flex justify-center items-center">
+    <h1 className='font-bold text-2xl text-white mt-2'>Something went wrong. Please try again.</h1>
+  </div>
 );
 
 export default Error;
