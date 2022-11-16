4e960e1396141bb378dd0866dbaf664168b9ab41
preciousimo <preciousimoniakemu@gmail.com>
2022-11-16 19:12:54 +0100

    Create skelenton discover section

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,35 @@
-const Discover = () => <div>Discover</div>;
+import { Error, Loader, SongCard } from "../components";
+import { genres } from "../assets/constants";
+
+const Discover = () => {
+  const genreTitle = 'Pop';
+
+  return (
+    <div className="flex flex-col">
+
+        {/* Wrapper for top side */}
+        <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+            <h2 className="font-bold text-3xl text-white">Discover {genreTitle} </h2>
+            <select 
+            onChange={() => {}}
+            value=""
+            className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5">
+                {genres.map((genre) => <option key={genre.value} value={genre.value}> {genre.title} </option>)}
+            </select>
+        </div>
+
+        {/* Wrappper for songs */}
+        <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
+                <SongCard 
+                    key={song.key}
+                    song={song}
+                    i={i}
+                />
+            ))}
+        </div>
+    </div>
+  );
+};
 
 export default Discover;
