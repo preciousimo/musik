af6f75012e24bf373b8e3d03adda32c8ad7f41ce
preciousimo <preciousimoniakemu@gmail.com>
2022-11-17 08:49:26 +0100

    Top chart card

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -11,6 +11,12 @@ import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 import 'swiper/css';
 import 'swiper/css/free-mode';
 
+const TopChartCard = ({ song, i }) => (
+  <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
+    {song.title}
+  </div>
+)
+
 const TopPlay = () => {
   const dispatch = useDispatch();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
@@ -44,7 +50,11 @@ const TopPlay = () => {
 
         <div className="mt-4 flex flex-col gap-1">
           {topPlays?.map((song, i) => (
-            <TopChartCard />
+            <TopChartCard
+              key={song.key}
+              song={song}
+              i={i}
+            />
           ))}
         </div>
       </div>
