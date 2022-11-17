c15a32e3a1b84a734bb79e1d75f0ea2e6a59fe17
preciousimo <preciousimoniakemu@gmail.com>
2022-11-17 17:59:35 +0100

    Song details page

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -11,10 +11,22 @@ export const shazamCoreApi = createApi({
         },
     }),
     endpoints: (builder) => ({
-        getTopCharts: builder.query({ query: () => '/charts/world' })
+        getTopCharts: builder.query({ query: () => '/charts/world' }),
+        getSongsByGenre: builder.query({ query: (genre) => `/charts/genre-world?genre_code=${genre}` }),
+        getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
+        getSongsBySearch: builder.query({ query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
+        getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
+        getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
+        getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
     }),
 });
 
 export const {
     useGetTopChartsQuery,
+    useGetSongsByGenreQuery,
+    useGetSongsByCountryQuery,
+    useGetSongsBySearchQuery,
+    useGetArtistDetailsQuery,
+    useGetSongDetailsQuery,
+    useGetSongRelatedQuery,
 } = shazamCoreApi;
